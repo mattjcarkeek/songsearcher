55a7df09597647dd79d4026af32aee6754416864
mattjcarkeek <114029419+mattjcarkeek@users.noreply.github.com>
2024-10-17 13:49:52 +1300

    a

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppComponent } from './app/app.component';
 import { Routes, provideRouter } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
   { path: '**', redirectTo: '' }
 ];
 
